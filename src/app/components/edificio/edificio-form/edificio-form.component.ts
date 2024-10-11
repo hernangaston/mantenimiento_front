a20be22b7770b890a34261dd6ea11fd413d76235
hernangaston <hernangaston@gmail.com>
2024-10-11 10:20:47 -0300

    nuevo service edificio

--- src/app/components/edificio/edificio-form/edificio-form.component.ts
+++ src/app/components/edificio/edificio-form/edificio-form.component.ts
@@ -1,8 +1,9 @@
-// src/app/components/edificio-form/edificio-form.component.ts
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Edificio } from '../../../models/edificio';
 import { PisoService } from '../../../service/piso.service';
+import { EdificioService } from '../../../service/edificio.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edificio-form',
@@ -13,7 +14,7 @@ export class EdificioFormComponent implements OnInit {
   edificioForm: FormGroup;
   pisos: any[] = [];
 
-  constructor(private formBuilder: FormBuilder, private pisoService: PisoService) {
+  constructor(private formBuilder: FormBuilder, private pisoService: PisoService, private edificioService: EdificioService, private router: Router ) {
     this.edificioForm = this.formBuilder.group({
       Nombre: ['', [Validators.required, Validators.maxLength(255)]],
       Direccion: ['', [Validators.required, Validators.maxLength(255)]],
@@ -28,17 +29,27 @@ export class EdificioFormComponent implements OnInit {
   }
 
   cargarPisos() {
-    this.pisoService.obtenerPisos().subscribe((data) => {
-      this.pisos = data;
-    }, (error) => {
-      console.error('Error al cargar los pisos:', error);
+    this.pisoService.obtenerPisos().subscribe({
+      next: (res)=>{
+        this.pisos = res;
+      },
+      error: (err) => {
+        console.error('Error al cargar los pisos:', err);
+      }
     });
   }
+
   onSubmit() {
     if (this.edificioForm.valid) {
-      const nuevoEdificio: Edificio = this.edificioForm.value;
-      console.log('Formulario válido, datos del edificio:', nuevoEdificio);
-      // Aquí puedes realizar una solicitud HTTP para enviar los datos al backend
+      const nuevoEdificio: Edificio = this.edificioForm.value;      
+      this.edificioService.crearEdificio(nuevoEdificio).subscribe({
+        next: (response) => {
+          console.log('Edificio creado con éxito:', response);
+          this.edificioForm.reset();  // Reinicia el formulario tras el éxito
+        },
+        error: (error) => { console.error('Error al crear el edificio:', error); },
+        complete: () => { this.router.navigate(['/dashboard']); }
+      });
     } else {
       console.log('Formulario no válido');
     }
